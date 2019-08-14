34b47f4c67fef7aa52e3ccfbe9b412ae591952cf
Gabe Arnold <gabrielgarnold@gmail.com>
2019-08-14 15:15:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
    if(isset($_POST['search']))
    {
        $valueToSearch = $_POST['valueToSearch'];
-       $query = "SELECT * FROM table_1 WHERE CONCAT(Course_ID, Course_Name, Days, Times, Instructor, Room, Units, Seats, Enrolled, Available) LIKE '%".$valueToSearch."%'";
+       $query = "SELECT * FROM table_2 WHERE CONCAT(CourseID, CourseName, Days, TimeStart, TimeEnd, Instructor, Room, Units, TotalEnrolled, RoomType) LIKE '%".$valueToSearch."%' AND CourseID != ''";
        $connect = mysqli_connect("localhost", "root", "", "csvimport");
        $search_result = mysqli_query($connect, $query);
        if (!$search_result) {
@@ -11,8 +11,20 @@
        
        }
    }
+
+    elseif(isset($_POST['timesearch'])) {
+       $connect = mysqli_connect("localhost", "root", "", "csvimport");
+       $time1 = $_POST['time1'];
+       $time2 = $_POST['time2'];
+       $query = "SELECT * FROM table_2 WHERE CAST(TimeStart AS time) BETWEEN '". mysqli_real_escape_string($connect, $time1)  ."' AND '". mysqli_real_escape_string($connect, $time2)  ."' ORDER BY TimeStart";
+       $search_result = mysqli_query($connect, $query);
+       if (!$search_result) {
+       printf("Error: %s\n", mysqli_error($connect));
+       exit();
+   }
+   }
     else {
-       $query = "SELECT * FROM table_1";
+       $query = "SELECT * FROM table_2 WHERE CourseID != ''";
        $connect = mysqli_connect("localhost", "root", "", "csvimport");
        $search_result = mysqli_query($connect, $query);
        if (!$search_result) {
@@ -46,10 +58,48 @@
                <input type="submit" name="search" value="Search">
                <a href="http://localhost/csFinalProject/"><button>Clear</button></a><br><br>
             </div>
-            <!--<div class="filter">
+            <div class="filter">
                <h2>Availability Search</h2>
-               <input type="text" id="search" placeholder="Type to search">
-               </div>-->
+               <b>Start Time From - To</b><br>
+                <select name="time1">
+                    <option value="07:00:00">7:00 AM</option>
+                    <option value="08:00:00">8:00 AM</option>
+                    <option value="09:00:00">9:00 AM</option>
+                    <option value="10:00:00">10:00 AM</option>
+                    <option value="11:00:00">11:00 AM</option>
+                    <option value="12:00:00">12:00 PM</option>
+                    <option value="13:00:00">1:00 PM</option>
+                    <option value="14:00:00">2:00 PM</option>
+                    <option value="15:00:00">3:00 PM</option>
+                    <option value="16:00:00">4:00 PM</option>
+                    <option value="17:00:00">5:00 PM</option>
+                    <option value="18:00:00">6:00 PM</option>
+                    <option value="19:00:00">7:00 PM</option>
+                    <option value="20:00:00">8:00 PM</option>
+                    <option value="21:00:00">9:00 PM</option>
+                    <option value="22:00:00">10:00 PM</option>
+                </select>
+                <select name="time2">
+                    <option value="07:00:00">7:00 AM</option>
+                    <option value="08:00:00">8:00 AM</option>
+                    <option value="09:00:00">9:00 AM</option>
+                    <option value="10:00:00">10:00 AM</option>
+                    <option value="11:00:00">11:00 AM</option>
+                    <option value="12:00:00">12:00 PM</option>
+                    <option value="13:00:00">1:00 PM</option>
+                    <option value="14:00:00">2:00 PM</option>
+                    <option value="15:00:00">3:00 PM</option>
+                    <option value="16:00:00">4:00 PM</option>
+                    <option value="17:00:00">5:00 PM</option>
+                    <option value="18:00:00">6:00 PM</option>
+                    <option value="19:00:00">7:00 PM</option>
+                    <option value="20:00:00">8:00 PM</option>
+                    <option value="21:00:00">9:00 PM</option>
+                    <option value="22:00:00">10:00 PM</option>
+                </select><br><br>
+                <input type="submit" name="timesearch" value="Search">
+                <a href="http://localhost/csFinalProject/"><button>Clear</button></a><br><br>
+            </div>
             <div class="courseList">
                <h2>Course List</h2>
                <table class="table table-bordered table-hover table-condensed" ; style="border:1px solid black;">
@@ -58,13 +108,13 @@
                         <th title="Field #1" align="left">Course ID</th>
                         <th title="Field #2" align="left">Course Name</th>
                         <th title="Field #3" align="left">Days</th>
-                        <th title="Field #4" align="left">Times</th>
-                        <th title="Field #5" align="left">Instructor</th>
-                        <th title="Field #6" align="left">Room</th>
-                        <th title="Field #7" align="left">Units</th>
-                        <th title="Field #8" align="left">Seats</th>
+                        <th title="Field #4" align="left">Start Time</th>
+                        <th title="Field #5" align="left">End Time</th>
+                        <th title="Field #6" align="left">Instructor</th>
+                        <th title="Field #7" align="left">Room</th>
+                        <th title="Field #8" align="left">Units</th>
                         <th title="Field #9" align="left">Enrolled</th>
-                        <th title="Field #10" align="left">Available</th>
+                        <th title="Field #10" align="left">Classroom Type</th>
                         <th title="Field #11" align="left">EDIT</th>
                      </tr>
                   </thead>
@@ -84,16 +134,16 @@
                             }
                             echo
                             "<tr class=$rclass>
-                                <td>{$row['Course_ID']}</td>
-                                <td>{$row['Course_Name']}</td>
+                                <td>{$row['CourseID']}</td>
+                                <td>{$row['CourseName']}</td>
                                 <td>{$row['Days']}</td>
-                                <td>{$row['Times']}</td>
+                                <td>{$row['TimeStart']}</td>
+                                <td>{$row['TimeEnd']}</td>
                                 <td>{$row['Instructor']}</td>
                                 <td>{$row['Room']}</td>
                                 <td>{$row['Units']}</td>
-                                <td>{$row['Seats']}</td>
-                                <td>{$row['Enrolled']}</td>
-                                <td>{$row['Available']}</td>
+                                <td>{$row['TotalEnrolled']}</td>
+                                <td>{$row['RoomType']}</td>
                                 <td><a href='edit.php?edit=$num'>edit</a></td>
                             </tr>";
                             $num = $num + 1;
